a76cbf03ae725ec244e036e6df5f49a83d185075
Afraz <mukeshbhandaris>
2021-10-25 17:51:09 +0530

    changes

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer",  "Designer", "Freelancer", "Designer"],
+        strings: ["Developer",  "Designer", "Freelancer", "Designer", "Andriod Apps"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Freelancer","Designer"],
+        strings: ["Developer", "Designer", "Freelancer","Designer","Andriod Apps"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
