00ec5beaa68d519677181b537d9a4442506a5722
Afraz <mukeshbhandaris>
2021-10-25 18:30:18 +0530

    sdsd

--- script.js
+++ script.js
@@ -31,13 +31,13 @@ $(document).ready(function(){
     });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Developer",  "Designer", "Freelancer", "Designer", "Andriod App developement "],
+        strings: ["Developer",  "Designer", "Freelancer", "Designer", "Andriod App developer "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Freelancer","Designer","Andriod App developement"],
+        strings: ["Developer", "Designer", "Freelancer","Designer","Andriod App developer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
